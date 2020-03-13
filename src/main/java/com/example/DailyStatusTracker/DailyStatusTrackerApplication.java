adaf6d5f5a8bba839b154cd17a8c061c30f437ac
Ankit Bhasin <ankit.bhasin10@gmail.com>
2020-03-14 01:00:06 +0530

    first commit

---
+++ src/main/java/com/example/DailyStatusTracker/DailyStatusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DailyStatusTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStatusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyStatusTrackerApplication.class, args);
+	}
+
+}
